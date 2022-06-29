32e1f26b38ef3f2dc5c5768945d2507578f85aef
Sylvain Garcia <garcia.6l20@gmail.com>
2022-06-29 05:52:36 +0200

    include testing only when BUILD_TESTING is enabled

--- G6.cmake
+++ G6.cmake
@@ -3,6 +3,9 @@ set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} PARENT_SCOPE)
 
 set(G6_CMAKE ON)
 
-include(G6Testing)
 include(G6Warnings)
 include(G6Sanitizers)
+
+if(BUILD_TESTING)
+    include(G6Testing)
+endif()
